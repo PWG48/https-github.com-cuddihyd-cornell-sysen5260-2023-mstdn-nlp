d5c4403573895a91c36e75e9dc61ee969e69d135
andrew-duffy10 <andrew.duffy97@gmail.com>
2023-05-01 12:56:56 -0400

    Adding rest api queries

--- rest/mnapi/main.py
+++ rest/mnapi/main.py
@@ -31,3 +31,58 @@ def spark_test():
         return list_of_dicts
     finally:
         spark_session.stop()
+
+@app.get("/mstdn-nlp/api/vi/accounts/")
+def spark_accounts():
+    from pyspark.sql import SparkSession
+    spark_session = SparkSession.builder\
+    .appName("rest-accounts")\
+    .master("spark://spark-master:7077")\
+    .config("spark.cores.max", 2)\
+    .getOrCreate()
+    try:
+        wc2 = spark_session.read.parquet('/opt/warehouse/TDIDF.parquet/')
+        wc2.createOrReplaceTempView("TDIDF")
+        query = "SELECT username, id FROM TDIDF"
+        ans = spark_session.sql(query)
+        list_of_dicts = ans.rdd.map(lambda row: row.asDict()).collect()
+        return list_of_dicts
+    finally:
+        spark_session.stop()
+
+@app.get("/api/v1/tf-idf/user-ids/{user_id}")
+def spark_user(user_id: int):
+    from pyspark.sql import SparkSession
+    spark_session = SparkSession.builder\
+    .appName("rest-user")\
+    .master("spark://spark-master:7077")\
+    .config("spark.cores.max", 2)\
+    .getOrCreate()
+    try:
+        wc2 = spark_session.read.parquet('/opt/warehouse/TDIDF.parquet/')
+        wc2.createOrReplaceTempView("TDIDF")
+        query = "SELECT * FROM TDIDF WHERE id = '{user_id}'"
+        ans = spark_session.sql(query)
+        list_of_dicts = ans.rdd.map(lambda row: row.asDict()).collect()
+        return list_of_dicts
+    finally:
+        spark_session.stop()
+
+@app.get("/api/v1/tf-idf/user-ids/{user_id}/neighbors")
+def spark_neighbors(user_id: int):
+    from pyspark.sql import SparkSession
+    spark_session = SparkSession.builder\
+    .appName("rest-neighbors")\
+    .master("spark://spark-master:7077")\
+    .config("spark.cores.max", 2)\
+    .getOrCreate()
+    try:
+        wc2 = spark_session.read.parquet('/opt/warehouse/TDIDF.parquet/')
+        wc2.createOrReplaceTempView("TDIDF")
+        query = "SELECT * FROM TDIDF WHERE id = '{user_id}'"
+        ans = spark_session.sql(query).limit(10)
+        list_of_dicts = ans.rdd.map(lambda row: row.asDict()).collect()
+        return list_of_dicts
+    finally:
+        spark_session.stop()
+
